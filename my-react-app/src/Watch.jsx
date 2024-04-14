73d344d688e8afc44f28e7759767a984a29afdce
Sujay Subramanyam <ssubr022@gmail.com>
2024-04-13 20:49:54 -0700

    keep working

--- my-react-app/src/Watch.jsx
+++ my-react-app/src/Watch.jsx
@@ -5,7 +5,83 @@ const Watch = () => {
   return (
     <div className="watch">
       <div className='section'>
+        <h1>Category of Content</h1>
         <div className='layout'>
+          <button>
+            <div className="show-tile">            
+            </div>
+          </button>
+          <button>
+            <div className="show-tile">            
+            </div>
+          </button>
+          <button>
+            <div className="show-tile">            
+            </div>
+          </button>
+          <button>
+            <div className="show-tile">            
+            </div>
+          </button>
+        </div>
+      </div>
+      <div className='section'>
+        <h1>Category of Content</h1>
+        <div className='layout'>
+          <button>
+            <div className="show-tile">            
+            </div>
+          </button>
+          <button>
+            <div className="show-tile">            
+            </div>
+          </button>
+          <button>
+            <div className="show-tile">            
+            </div>
+          </button>
+          <button>
+            <div className="show-tile">            
+            </div>
+          </button>
+        </div>
+      </div>
+      <div className='section'>
+        <h1>Category of Content</h1>
+        <div className='layout'>
+          <button>
+            <div className="show-tile">            
+            </div>
+          </button>
+          <button>
+            <div className="show-tile">            
+            </div>
+          </button>
+          <button>
+            <div className="show-tile">            
+            </div>
+          </button>
+          <button>
+            <div className="show-tile">            
+            </div>
+          </button>
+        </div>
+      </div>
+      <div className='section'>
+        <h1>Category of Content</h1>
+        <div className='layout'>
+          <button>
+            <div className="show-tile">            
+            </div>
+          </button>
+          <button>
+            <div className="show-tile">            
+            </div>
+          </button>
+          <button>
+            <div className="show-tile">            
+            </div>
+          </button>
           <button>
             <div className="show-tile">            
             </div>
